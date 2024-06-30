630e9b2d0f1a16167235c454b85fde60ce55892b
me-junaid <140057488+me-junaid@users.noreply.github.com>
2024-07-01 00:01:30 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,15 +3,13 @@ from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, CallbackContext
 
 def start(update: Update, context: CallbackContext) -> None:
-    # Define the button with a URL
     keyboard = [
-        [InlineKeyboardButton("Visit Webapp", url='https://t.me/BankthriftBot/store')]
+        [InlineKeyboardButton("Visit Store", url='https://t.me/BankthriftBot/store')]
     ]
 
     reply_markup = InlineKeyboardMarkup(keyboard)
 
-    # Send the message with the button
-    update.message.reply_text('Click the button below to visit our web app:', reply_markup=reply_markup)
+    update.message.reply_text('Wanna thrift?', reply_markup=reply_markup)
 
 def main():
     token = os.getenv('TELEGRAM_TOKEN')
